11fd5138b179f75a55b3ff95ab1568c8345593ba
alp161093 <alvaroldp93@gmail.com>
2026-02-12 10:49:47 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/pronosticup/backend/PronosticupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pronosticup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PronosticupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PronosticupBackendApplication.class, args);
+	}
+
+}
